4512bb49e31e8fa0b118e8089407a024c1d247b6
xbtsw <xbtsw.chen@gmail.com>
2015-03-03 21:04:29 +0000

    bug fix and api change

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 var Make = require('./lib/make.js').Make;
 
-module.exports = (function(){
+module.exports = (function() {
     var make = new Make();
     make.extraArgs = process.argv.slice(3);
-    
+
     return {
-        define: make.define.bind(make),
-        depends: make.define.bind(make),
+        rule: make.rule.bind(make),
+        run: make.run.bind(make),
         cwd: make.cwd.bind(make),
         args: make.args.bind(make),
-        run: make.run.bind(make)
+        Make: Make
     };
 })();
